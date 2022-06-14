e6a8ef904799f8d089cbdbd1f2450f00b4d63bfb
Azarill <heierms@gmail.com>
2022-06-13 23:43:02 -0500

    added new associations

--- models/index.js
+++ models/index.js
@@ -17,9 +17,19 @@ Product.belongsTo(Category,{
 });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+    through: ProductTag,
+    as: 'product_tag',
+    foreignKey: 'tag_id'
+});
 
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+    through: ProductTag,
+    as: 'product_tag',
+    foreignKey: 'product_id'
+})
 
 
 module.exports = {
